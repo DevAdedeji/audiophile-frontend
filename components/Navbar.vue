80a442c7be9634ce70e6827eb4ac8ad7262ec4d6
DevAdedeji <adedejitewogbade2@gmail.com>
2024-01-14 21:13:35 +0100

    Removed nuxt-image

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,14 +3,14 @@
     class="fixed top-0 right-0 left-0 h-10vh flex items-center justify-between w-90% mx-auto max-wid"
   >
     <div class="flex items-center">
-      <NuxtImg
-        src="/icons/hamburger.svg"
+      <img
+        src="../assets/icons/hamburger.svg"
         class="block mr-10 lg:hidden"
         width="16"
         height="15"
       />
 
-      <NuxtImg src="../assets/images/logo.png" width="143" height="25" />
+      <img src="../assets/images/logo.png" width="143" height="25" />
     </div>
     <ul class="hidden items-center gap-6 list-none lg:flex">
       <li v-for="link in links" :key="link.title">
@@ -22,7 +22,7 @@
       </li>
     </ul>
     <NuxtLink to="/" class="pr-4">
-      <NuxtImg src="/icons/cart.svg" width="23.33" height="20" />
+      <img src="../assets/icons/cart.svg" width="23.33" height="20" />
     </NuxtLink>
   </nav>
 </template>
